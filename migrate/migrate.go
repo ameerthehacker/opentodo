1256b5bf52633dbe7619c5fc1e956998b7a0f015
Ameer Jhan <ameerjhanprof@gmail.com>
2020-06-24 12:27:49 +0530

    feat: start with basic cli

---
+++ migrate/migrate.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command {
+		{
+			Name: "create",
+			Description: "Create a new migration",
+			Action: func(context * cli.Context) {
+				migrationName := context.Args().Get(0)
+
+				fmt.Println(migrationName)
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
